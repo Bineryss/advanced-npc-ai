8207ceca4b13f3dbdb14f506ba1d67a9b50dfff4
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2025-03-22 20:34:15 +0000

    add needs recution logic

--- src/npc-logic.test.ts
+++ src/npc-logic.test.ts
@@ -1,17 +1,35 @@
-import { createNPC } from "./npc-logic"
+import { createNPC, updateNeeds } from "./npc-logic"
 
 describe('npc logic', () => {
     it('should create a npc', () => {
         const npc = createNPC('bob', 'under the bridge')
-        console.log(npc)
         expect(npc).toStrictEqual({
             name: 'bob',
             needs: { sleep: 100, food: 100, social: 100 },
             location: {
-              home: 'under the bridge',
-              current: 'under the bridge',
-              destination: undefined
+                home: 'under the bridge',
+                current: 'under the bridge',
+                destination: undefined
             }
+        })
+    })
+    it('should reduce needs', () => {
+        const npc = createNPC('bob', 'under the bridge')
+        const updatedNpc = updateNeeds(npc)
+        expect(updatedNpc).toStrictEqual({
+            ...npc,
+            needs: { sleep: 90, food: 90, social: 90 },
+        })
+    })
+    it('should never reduce needs below 0', () => {
+        const npc = createNPC('bob', 'under the bridge')
+        let updatedNpc = npc;
+        for (let i = 0; i < 11; i++) {
+            updatedNpc = updateNeeds(updatedNpc)
+        }
+        expect(updatedNpc).toStrictEqual({
+            ...npc,
+            needs: { sleep: 0, food: 0, social: 0 },
         })
     })
 })
--- src/npc-logic.ts
+++ src/npc-logic.ts
@@ -15,3 +15,17 @@ export function createNPC(name: string, homeLocation: string): Npc {
         }
     }
 }
+
+export function updateNeeds(npc: Npc): Npc {
+    const needMultiplier = 10
+
+    return {
+        ...npc,
+        needs: {
+            ...npc.needs,
+            sleep: Math.max(0, npc.needs.sleep - 1 * needMultiplier),
+            food: Math.max(0, npc.needs.food - 1 * needMultiplier),
+            social: Math.max(0, npc.needs.social - 1 * needMultiplier)
+        }
+    }
+}
