6451f505fe2b8b97654ca42c2ecd3cce04684c4e
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2025-03-22 22:20:03 +0000

    improve type safety on withParams

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,3 @@
-import { log } from "console"
 import { pipe, withParams } from "./utils"
 
 describe('utils', () => {
@@ -18,17 +17,17 @@ describe('utils', () => {
     it.each([
         {
             functionWithParams: divideBy10,
-            params: [],
+            params: [] as const,
             expected: 1
         },
         {
             functionWithParams: (value: number, add: number) => value + add,
-            params: [10],
+            params: [10] as const,
             expected: 20
         },
         {
             functionWithParams: (value: number, add: number, add2: number) => value + add + add2,
-            params: [10, 22],
+            params: [10, 22] as const,
             expected: 42
         },
     ])('should wrapp function with $params.length addition parameters', ({ functionWithParams, params, expected }) => {
@@ -37,12 +36,12 @@ describe('utils', () => {
         expect(wrapped(10)).toBe(expected)
     })
     it('should work with multi param functions', () => {
-        const withTwoParams = (value: number, add: string) => value + add
+        const withTwoParams = (value: number, add: string) => value + Number.parseInt(add)
         const value: number = pipe(10,
             (value) => value * 2,
             withParams(withTwoParams, '12')
         )
-        console.log(value)
-        console.log(typeof value)
+
+        expect(value).toBe(32)
     })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,10 @@
 type PipeFunction<Input, Output> = (value: Input) => Output
+type ExcludeFirstParam<T extends (...args: any[]) => any> = T extends (first: any, ...rest: infer R) => any ? R : never;
 
 export function pipe<T>(value: T, ...functions: PipeFunction<T, T>[]): T {
     return functions.reduce((total: T, nextFunction: PipeFunction<T, T>) => nextFunction(total), value)
 }
 
-export function withParams<T>(callFunction: Function, ...params: unknown[]): PipeFunction<T, T> {
+export function withParams<T, F extends (...args: any[]) => T>(callFunction: F, ...params: ExcludeFirstParam<F>): PipeFunction<T, T> {
     return (value: T) => callFunction(value, ...params)
 }
